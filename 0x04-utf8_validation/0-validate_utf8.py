6ca8145ff20c13dddad79b9c0d4b89940242558b
LabyKreative <labykreative@yahoo.com>
2023-10-24 15:09:31 +0000

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,7 @@
 
 
 def validUTF8(data):
-    # Define a bitmask to check the most significant bits of the bytes
+    """Defines a bitmask to check the most significant bits of the bytes"""
     mask1 = 1 << 7
     mask2 = 1 << 6
 
@@ -11,24 +11,28 @@ def validUTF8(data):
     count = 0
 
     for byte in data:
+
+        mask_byte = 1 << 7
+
         if count == 0:
-            if byte & mask1 == 0:
-                continue
-            # Count the number of leading 1s to determine the byte length
-            while byte & mask1:
+
+            while mask_byte & byte:
                 count += 1
-                byte <<= 1
-            # Invalid encoding if the length is not in [1, 4]
-            if count == 1 or count > 4:
-                return False
-            # For a single-byte character, count remains 0
+                mask_byte = mask_byte >> 1
+
             if count == 0:
                 continue
-        else:
-            # Check if the byte is a valid trailing byte
-            if not (byte & mask1 and not byte & mask2):
-                return False
-            count -= 1
 
-    # All bytes have been checked, and the count should be 0 if it's valid UTF-8
-    return count == 0
+            if count == 1 or count > 4:
+                return False
+
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        count -= 1
+
+    if count == 0:
+        return True
+
+    return False
